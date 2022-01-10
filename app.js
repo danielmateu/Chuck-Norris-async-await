e9590e61babfc586d129311f29b8fa0cf545a6c6
Dani <danielmateu86@gmail.com>
2022-01-10 11:30:24 +0100

    Metodo tradicional de utilizar fetch mediante promesas

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function getData(){
     fetch('https://api.chucknorris.io/jokes/random')
     .then(resultado => resultado.json())
     .then(data => data.value)
+    .catch(err => console.log(err));
 }
 
 getData();
