232d1694b01d7619f05f36342c0c230e4e8515ec
Dani <danielmateu86@gmail.com>
2022-01-10 11:45:55 +0100

    inyectamos el valor al HTML

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict'
 
+document.getElementById('bromaBoton').addEventListener('click', getData())
+
+const bromaContenedor = document.querySelector('#bromaContenedor')
+
 //Metodo tradicional de utilizar fetch mediante promesas.
 
 function getData() {
     fetch('https://api.chucknorris.io/jokes/random')
         .then(resultado => resultado.json())
-        .then(data => data.value)
+        .then(data => {
+            bromaContenedor.innerText = data.value;
+        })
         .catch(err => console.log(err));
 }
 
