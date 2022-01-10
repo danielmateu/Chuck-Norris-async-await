11d0e07271dce8e52ba8c9cfd0c34fcc0fc22fb8
Dani <danielmateu86@gmail.com>
2022-01-10 11:40:07 +0100

    Forma moderna async-await

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 'use strict'
 
-function getData(){
+//Metodo tradicional de utilizar fetch mediante promesas.
+
+function getData() {
     fetch('https://api.chucknorris.io/jokes/random')
-    .then(resultado => resultado.json())
-    .then(data => data.value)
-    .catch(err => console.log(err));
+        .then(resultado => resultado.json())
+        .then(data => data.value)
+        .catch(err => console.log(err));
 }
 
 getData();
+
+// Forma moderna async-await
+
+async function getDataAsyncAwait() {
+    try {
+        const resultado = await fetch('https://api.chucknorris.io/jokes/random');
+        const data = await resultado.json();
+        //console.log(data.value);
+    } catch(err){
+        console.log(err);
+    }
+
+}
+
+getDataAsyncAwait();
